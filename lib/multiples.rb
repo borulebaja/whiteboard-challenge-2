d21ee51bc38fd4086f01fd679de711d7bdcd9c85
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-23 11:52:49 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    (1...limit).to_a #to colllect num below 1000 in array
+   (1...limit).select{ |i| (i % 5 == 0 || i % 3 == 0)}   
+end 
+
+
+def sum_multiples(limit)
+     sum = 0
+     multiples = collect_multiples(limit)
+   multiples.each { |i| sum += i }
+    sum
+end
+
+
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+       sum = Array.new
+       limit.times do |number|
+            if number != 0 && (number % 5 == 0 || number % 3 == 0)
+                sum << number
+            end
+        end
+      sum
+    end
+
+    def sum_multiples
+        (1...limit).select{|i|i%3==0 or i%5==0}.inject(:+)
+    end
+
+end
